9a8063e0c87c694145f6759caa2a6dd7ddbdc7b7
Banid Prajapati <banidprajapati98@gmail.com>
2025-11-22 21:39:15 -0800

    add kokoro and mongo routes

--- storytelling_videos/routers/_base.py
+++ storytelling_videos/routers/_base.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from storytelling_videos.routers.openrouter_router import router as OpenRouter
+from storytelling_videos.routers.kokoro_tts_router import router as KokoroRouter
+from storytelling_videos.routers.mongo_router import router as MongoRouter
 
 # Create a combined router for all routes
 router = APIRouter()
-router.include_router(OpenRouter, prefix="/stories", tags=["stories"])
+router.include_router(MongoRouter, prefix="/stories", tags=["stories"])
+router.include_router(KokoroRouter, prefix="/tts", tags=["TTS"])
 
 __all__ = ["router"]
